8e9c5532f50866fb2ff2dd59e55d4ccd751f34f5
64010248Nattapon <nattapon.nut.wp@gmail.com>
2025-10-22 00:19:46 +0700

    up

--- frontend/src/screens/index.tsx
+++ frontend/src/screens/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MasterGoalScreen } from "./screens/MasterGoalScreen";
+import { MasterGoalScreen } from "../screens/MasterGoalScreen";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
